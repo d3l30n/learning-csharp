8933836ebc95361d14bc7e6ff634bc94742f7440
Deleon <yougomylad@gmail.com>
2026-03-18 10:46:22 -0300

    first-uri

---
+++ uri-1001.cs
@@ -0,0 +1,23 @@
+﻿namespace learning_csharp;
+
+public class Program
+{
+  
+  public static void Main()
+  { 
+    int  n1, n2, sum;
+   Console.WriteLine("First number:");
+   n1 = Convert.ToInt32(Console.ReadLine());
+
+   Console.WriteLine("Second number:");
+   n2 = Convert.ToInt32(Console.ReadLine());
+
+   sum = n1 + n2;
+
+   Console.WriteLine("The sum of these two numbers is " + sum);
+
+  }
+
+
+
+}
