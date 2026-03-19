4695de5ce6cacd19f1899e1c7aea824bcba8fdf7
Deleon <yougomylad@gmail.com>
2026-03-19 15:29:35 -0300

    new exercises

---
+++ 03/uri-1003.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class uri1003
+{
+
+    static void Main(){
+
+        int a, b, x;
+
+       Console.WriteLine("Enter the first number: ");
+       a = Convert.ToInt32(Console.ReadLine());
+
+       Console.WriteLine("Enter the second number: ");
+       b = Convert.ToInt32(Console.ReadLine());
+
+       x = a + b;
+
+       Console.WriteLine("Sum = " + x);
+    }
+}
---
+++ 04/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+int a, b, prod;
+
+Console.WriteLine("A = ");
+a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("B = ");
+b = Convert.ToInt32(Console.ReadLine());
+
+prod = a * b;
+
+Console.WriteLine("Prod = " + prod);
