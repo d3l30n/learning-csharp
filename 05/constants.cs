f1505ef2c2239636e06f0f3596f8551e25a89991
Deleon <yougomylad@gmail.com>
2026-03-20 10:12:25 -0300

    new exercises

---
+++ 05/constants.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class constants
+{
+
+    static void Main()
+    {
+        //negative testing on constant variables
+        const string song = "It was a bad idea";
+        const string song1 = "I can't get enough";
+
+        Console.WriteLine("{0} \n To think I could stop \n Was such a bad idea \n {1}", song, song1);
+        //below theres an intentional error (since constant variables cannot have their value changed after the first assignment)
+        //string song = "whatever";
+
+        //Console.WriteLine("{0}\n Needing you so late \n Was such a bad idea \n 'Cause I can't think straight", song);
+
+    }
+
+
+
+}
---
+++ composite-formatting/composite-formatting.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class composite_formatting
+{
+
+    static void Main()
+    {
+        //Composite Formatting in C# is a feature that lets you insert values into a string using placeholders like {0}, {1}, which are replaced by provided arguments (e.g., Console.WriteLine("Result: {0}", x);).
+
+        int num = 10;
+        string name = "John Doe";
+        string name2 = "Jane Doe";
+
+        Console.Write("{1} was married to {2} for long {0} years.", num, name, name2);
+
+
+
+    }
+
+}
